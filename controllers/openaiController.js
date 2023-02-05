72062111b1ef93b173d6742e8d495f13db3b5009
Swatisharma175 <56503548+Swatisharma175@users.noreply.github.com>
2023-02-05 14:41:56 +0530

    Frontend completed

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -10,26 +10,25 @@ const openai = new OpenAIApi(configuration);
 
 const generateImage = async (req,res) => { 
     const { prompt , size } = req.body;
-
+    console.log("---------------------------------------------------------------");
+    console.log(req);
     const imageSize = 
-        size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024'; 
-
+        size === false ? '256x256' : '512x512';
 
     try{
-        console.log("hi");
         const response = await openai.createImage({
             prompt,
             n: 1,
             size: imageSize,
         });
         const imageUrl = response.data.data[0].url
-       
         res.status(200).json({
             success : true,
             data: imageUrl
         });
 
     }catch(error){
+        console.log(error)
         if (error.response) {
             console.log(error.response.status);
             console.log(error.response.data);
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+const path = require('path');
 const express = require('express');
+const cors = require('cors') // -<
 const dotenv  = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 // Enable body  parser
+app.use(cors()); // -< 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+// Set static folder
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use('/openai',require('./routes/openaiRoutes'));
 
