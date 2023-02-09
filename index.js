fa0d42656f774626db861751e1fe268f861d22ea
Swatisharma175 <56503548+Swatisharma175@users.noreply.github.com>
2023-02-09 16:15:35 +0530

    bug fixed

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 // Set static folder
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'/public/')))
 
 app.get('/backend',(req,res)=>{
     res.send("hello");
